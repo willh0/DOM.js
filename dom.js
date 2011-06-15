241235493464fa63ccf417bcaa1d06a0d7e20f4d
William Howard <williamhoward@willHome.local>
2011-06-15 10:30:36 +0100

    Fix singleton, minified version.

--- dom.js
+++ dom.js
@@ -4,7 +4,7 @@
 
 	Looks something like:
 
-	DIV({"class":"somClass"},
+	DIV({"class":"someClass"},
 		A({"href":"http://google.com"}, my link), SPAN(null,"some text")
 	);
  
@@ -26,7 +26,7 @@ var DOM = (function() {
 
 		if (args.length > 0) {
 			if (typeof(txt) === "undefined" || !txt) { //This is a singleton element.
-				return '<'+tag,attrsToStr(attrs)+' />';
+				return '<'+tag+attrsToStr(attrs)+' />';
 			} else {
 				return '<'+tag+attrsToStr(attrs)+'>'+args.slice(2).join("")+'</'+tag+'>';
 			}
@@ -37,7 +37,7 @@ var DOM = (function() {
 	/**
 	  Turns attributes object to a string.
 	  @function
-	  @param {Object} attrs Attributes to turn into an object.
+	  @param {Object} attrs Attributes to add to element (like: {"id":"someId", class:"someClass"}).
 		@returns {String} The attributes as a string.
 	 */
 	var attrsToStr = function(attrs) {
@@ -49,9 +49,10 @@ var DOM = (function() {
 	};
 
 	/**
-	  Creates a shortcut for creating DOM elements, like A({"href":"http://google.com"}, "link") instead of DOM.createDOM("a",{"href":"http://google.com"}, "link").
+	  Creates a partially applied function for creating DOM elements, like A({"href":"http://google.com"}, "link") instead of DOM.createDOM("a",{"href":"http://google.com"}, "link").
 	  @function
-	  @param {String} tag Name of the tag to create a shortcut for.
+	  @param {String} tag Name of the tag to create a shortcut for (like, "A", "DIV", "IMG").
+		@returns {Function}
 	 */
 	var DOMShortcut = function(tag) {
 		return function() {
@@ -66,20 +67,30 @@ var DOM = (function() {
 
 })();
 
-var A      = DOM.DOMShortcut("A"),
-    DIV    = DOM.DOMShortcut("DIV"),
-    SPAN   = DOM.DOMShortcut("SPAN"),
-    INPUT  = DOM.DOMShortcut("INPUT"),
-    TR     = DOM.DOMShortcut("TR"),
-    TD     = DOM.DOMShortcut("TD"),
-    TABLE  = DOM.DOMShortcut("TABLE"),
-    THEAD  = DOM.DOMShortcut("THEAD"),
-    TBODY  = DOM.DOMShortcut("TBODY"),
-    UL     = DOM.DOMShortcut("UL"),
-    LI     = DOM.DOMShortcut("LI"),
-    OL     = DOM.DOMShortcut("OL"),
-    P      = DOM.DOMShortcut("P"),
-    STRONG = DOM.DOMShortcut("STRONG"),
-    EM     = DOM.DOMShortcut("EM");
+var A					 = DOM.DOMShortcut("A"),
+    DIV				 = DOM.DOMShortcut("DIV"),
+    SPAN			 = DOM.DOMShortcut("SPAN"),
+    INPUT			 = DOM.DOMShortcut("INPUT"),
+		FORM			 = DOM.DOMShortcut("FORM"),
+    TABLE			 = DOM.DOMShortcut("TABLE"),
+    THEAD			 = DOM.DOMShortcut("THEAD"),
+    TBODY			 = DOM.DOMShortcut("TBODY"),
+		TR				 = DOM.DOMShortcut("TR"),
+		TD				 = DOM.DOMShortcut("TD"),
+		TFOOT			 = DOM.DOMShortcut("TFOOT"),
+		SELECT		 = DOM.DOMShortcut("SELECT"),
+		OPTION		 = DOM.DOMShortcut("OPTION"),
+		TEXTAREA   = DOM.DOMShortcut("TEXTAREA"),
+    UL				 = DOM.DOMShortcut("UL"),
+    LI				 = DOM.DOMShortcut("LI"),
+    OL				 = DOM.DOMShortcut("OL"),
+		H1				 = DOM.DOMShortcut("H1"),
+		H2				 = DOM.DOMShortcut("H2"),
+		H3				 = DOM.DOMShortcut("H3"),
+		H4				 = DOM.DOMShortcut("H4"),
+    P					 = DOM.DOMShortcut("P"),
+    STRONG		 = DOM.DOMShortcut("STRONG"),
+		IMG				 = DOM.DOMShortcut("IMG"),
+    EM				 = DOM.DOMShortcut("EM");
 
 
